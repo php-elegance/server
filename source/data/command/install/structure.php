f3c627c3ac57c9afbcc1ab3654ff0cc600ed3bb9
André Ricardo <contatoadmx@gmail.com>
2023-10-06 06:02:20 -0300

    library in install.structure

--- source/data/command/install/structure.php
+++ source/data/command/install/structure.php
@@ -6,14 +6,15 @@ namespace Elegance;
 
 return function () {
     Dir::create("app");
+    Dir::create("library");
+    Dir::create("library/assets");
+    Dir::create("routes");
     Dir::create("source");
     Dir::create("source/class");
     Dir::create("source/helper");
     Dir::create("source/helper/constant");
     Dir::create("source/helper/function");
     Dir::create("source/helper/script");
-    Dir::create("routes");
     Dir::create("view");
-
     Terminal::echo('Estrutura de pastas instalada');
 };
