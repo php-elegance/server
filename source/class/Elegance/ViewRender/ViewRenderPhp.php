a99a0fa83d34d10e95d13286de74594c05e4db31
André Ricardo <contatoadmx@gmail.com>
2023-10-06 23:51:36 -0300

    viewRenderPhp

---
+++ source/class/Elegance/ViewRender/ViewRenderPhp.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Elegance\ViewRender;
+
+abstract class ViewRenderPhp extends ViewRender
+{
+    protected static array $prepareReplace = [
+        '<!-- [#' => '[#',
+        '] -->' => ']',
+    ];
+
+    /** Aplica ações extras ao renderizar uma view */
+    protected static function renderizeAction(string $content, array $params = []): string
+    {
+        $content = str_replace(array_keys(self::$prepareReplace), array_values(self::$prepareReplace), $content);
+        $content = self::applyPrepare($content);
+
+        if (count(self::$current) == 1)
+            $content = self::organizeHtml($content);
+
+        return $content;
+    }
+
+    /** Retorna uma string HTML organizando as tags style e script */
+    static function organizeHtml(string $string): string
+    {
+        preg_match('/<html[^>]*>(.*?)<\/html>/s', $string, $html);
+
+        $string = count($html) ? self::organizeComplete($string) : self::organizePartial($string);
+
+        $string = str_replace_all(["\n\n", "\n ", "  ", "\r"], ["\n", "\n", ' ', ' '], trim($string));
+
+        return $string;
+    }
+
+    /** Aplica a organização em uma estrutura HTML parcial */
+    protected static function organizePartial(string $string): string
+    {
+        $src = [];
+        $script = [];
+        preg_match_all('/<script[^>]*>(.*?)<\/script>/s', $string, $tag);
+        $string = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $string);
+        foreach ($tag[1] as $key => $value)
+            if (empty(trim($value)))
+                $src[] = $tag[0][$key];
+            else
+                $script[] = $value;
+
+        $src = implode("\n", $src ?? []);
+        $script = implode("\n", $script ?? []);
+
+        if (!empty($script)) {
+            $script = ViewRenderJs::minify($script);
+            if (!empty($script))
+                $script = "<script>\n$script\n</script>";
+        }
+
+        preg_match_all('/<style[^>]*>(.*?)<\/style>/s', $string, $tag);
+        $string = preg_replace('#<style(.*?)>(.*?)</style>#is', '', $string);
+        $style = $tag[1];
+
+        $style = implode("\n", $style ?? []);
+
+        if (!empty($style)) {
+            $style = ViewRenderCss::minify($style);
+            if (!empty($style))
+                $style = "<style>$style</style>";
+        }
+
+        $string = [$src, $style, $string, $script];
+        $string = implode("\n", $string);
+
+        return $string;
+    }
+
+    /** Aplica a organização em uma estrutura HTML completa*/
+    protected static function organizeComplete(string $string): string
+    {
+        $src = [];
+        $script = [];
+        preg_match_all('/<script[^>]*>(.*?)<\/script>/s', $string, $tag);
+        $string = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $string);
+        foreach ($tag[1] as $key => $value)
+            if (empty(trim($value)))
+                $src[] = $tag[0][$key];
+            else
+                $script[] = $value;
+
+        $src = implode("\n", $src ?? []);
+        $script = implode("\n", $script ?? []);
+
+        if (!empty($script)) {
+            $script = ViewRenderJs::minify($script);
+            if (!empty($script))
+                $script = "<script>\n$script\n</script>";
+        }
+
+        preg_match_all('/<style[^>]*>(.*?)<\/style>/s', $string, $tag);
+        $string = preg_replace('#<style(.*?)>(.*?)</style>#is', '', $string);
+        $style = $tag[1];
+
+        $style = implode("\n", $style ?? []);
+
+        if (!empty($style)) {
+            $style = ViewRenderCss::minify($style);
+            if (!empty($style))
+                $style = "<style>\n$style\n</style>";
+        }
+
+        preg_match_all('/<head[^>]*>(.*?)<\/head>/s', $string, $tag);
+        $string = str_replace($tag[0], '[#head]', $string);
+        $string = preg_replace('#<head(.*?)>(.*?)</head>#is', '', $string);
+        $head = $tag[1];
+
+        $head[] = $style;
+        $head[] = $src;
+        $head[] = $script;
+
+        $head = implode("\n", $head);
+        $head = "<head>\n$head\n</head>";
+
+        $string = prepare($string, ['head' => $head]);
+
+        return $string;
+    }
+}
