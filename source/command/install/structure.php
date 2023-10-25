cda5148b739e5864cc5aa98abbf97a3b8e519dd6
André Ricardo <contatoadmx@gmail.com>
2023-10-25 02:24:54 -0300

    update install.structure

--- source/command/install/structure.php
+++ source/command/install/structure.php
@@ -5,15 +5,15 @@ namespace Elegance;
 // php mx install.structure
 
 return function () {
-    Dir::create("action");
-    Dir::create("assets");
+    Dir::create("app");
+    Dir::create("public");
     Dir::create("class");
-    Dir::create("helper");
-    Dir::create("helper/constant");
-    Dir::create("helper/function");
-    Dir::create("helper/script");
-    Dir::create("routes");
     Dir::create("source");
+    Dir::create("source/helper");
+    Dir::create("source/helper/constant");
+    Dir::create("source/helper/function");
+    Dir::create("source/helper/script");
+    Dir::create("source/routes");
     Dir::create("view");
     Terminal::echo('Estrutura de pastas instalada');
 };
