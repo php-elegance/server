c85ba0a41d339e10899b8f410b5e2b4f025b92bb
André Ricardo <contatoadmx@gmail.com>
2023-10-29 15:00:13 -0300

    update template index

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Elegance;
-
-Router::add([], [
-    'favicon.ico' => fn () => Assets::send('#elegance-server/public/favicon.ico'),
-    'assets...' => fn () => Assets::send('public/', ...Request::route()),
-    '' => 'home'
-]);
